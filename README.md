408d097486d1c0c2c7729442dd0f9f768b0e80af
Leo Orpilla III <hi@ldgrp.me>
2023-04-19 01:35:45 +1000

    Add sample data

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 A spreadsheet for calculating probabilities.
 See the [idea page][idea-page] for more information.
 
+Demo available at [https://probability-spreadsheet.vercel.app][demo].
+
 [idea-page]: https://ldgrp.me/ideas/probability-spreadsheet.html
+[demo]: https://probability-spreadsheet.vercel.app
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,23 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { NamespaceStore } from "./store/store";
+import { runInAction } from "mobx";
 
 const store = new NamespaceStore(10, 6);
+
+runInAction(() => {
+  store.cells.get("A2")!.formula = 'Tri Dist'
+  store.cells.get("B2")!.formula = '= triangular';
+
+  store.cells.get("A3")!.formula = 'Tri Dist + 1'
+  store.cells.get("B3")!.formula = '= B2 + 1';
+
+  store.cells.get("A5")!.formula = 'Uniform Dist'
+  store.cells.get("B5")!.formula = '= uniform';
+
+  store.cells.get("A6")!.formula = 'Uniform Dist + 1'
+  store.cells.get("B6")!.formula = '= B5 + 1';
+})
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
